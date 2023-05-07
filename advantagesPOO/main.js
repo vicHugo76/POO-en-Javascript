eae0168ef424a8f936ec9682da0414be0a651f3a
Victor Hugo <viccorrt@gmail.com>
2023-05-07 15:24:16 -0700

    En esta clase vimos la diferencias entre objeto literal y manejo de clases asi como sus ventajas y desventajas

---
+++ advantagesPOO/main.js
@@ -0,0 +1,147 @@
+// Objetos Literales - La gran desventaja es la creacion de un objeto para cada conjunto de datos, ademas que no es escalable por si tiene alguna modificacion en sus datos, en si no es buena idea usar este tipo de objetos o no es programacion orientada a objetos.
+const hugo = {
+  name: 'Victor Hugo',
+  username: 'vhugo',
+  point: 230,
+  socialMedia: {
+    twitter: 'vHugo76',
+    instagram: undefined,
+    facebook: 'Victor Ortiz'
+  },
+  approvedCourses: [
+    'Curso Definitivo de HTML y CSS',
+    'Curso Práctico de HTML y CSS'
+  ],
+  learningPaths: [
+    {
+      name: 'Escuela de Desarrollo Web',
+      courses: [
+        'Curso Definitivo de HTML y CSS',
+        'Curso Práctico de HTML y CSS',
+        'Curso de Responsive Design',
+        'Curso de Sistemas de Diseño'
+      ]
+    },
+    {
+      name: 'Escuela de Videojuegos',
+      courses: [
+        'Curso de Introducción a la Producción de Videojuegos',
+        'Curso de Unreal Engine',
+        'Curso de Unity 3D'
+      ]
+    }
+  ]
+}
+
+const jared = {
+  name: 'Oliver Jared',
+  username: 'oJared',
+  point: 130,
+  socialMedia: {
+    twitter: 'oJared04',
+    instagram: 'oJared04',
+    facebook: undefined
+  },
+  approvedCourses: [
+    'Curso de Física',
+    'Curso de Astronomía'
+  ],
+  learningPaths: [
+    {
+      name: 'Escuela de Astrofísica',
+      courses: [
+        'Curso de Física',
+        'Curso de Astronomía',
+        'Curso de Física Cuántica'
+      ]
+    },
+    {
+      name: 'Escuela de Videojuegos',
+      courses: [
+        'Curso de Introducción a la Producción de Videojuegos',
+        'Curso de Unreal Engine',
+        'Curso de Unity 3D'
+      ]
+    }
+  ]
+}
+
+// Programacion Orientada a Objetos
+class LearningPath {
+  constructor({
+    name,
+    courses = []
+  }) {
+    this.name = name;
+    this.courses = courses;
+  }
+  addCourses(...course) {
+    this.courses.push(...course);
+  }
+}
+
+const escuelaWeb = new LearningPath({name: 'Escuela de Desarrollo Web'});
+escuelaWeb.addCourses(
+  'Curso Definitivo de HTML y CSS',
+  'Curso Práctico de HTML y CSS',
+  'Curso de Responsive Design',
+  'Curso de Sistemas de Diseño'
+);
+const escuelaVgs = new LearningPath({name: 'Escuela de Videojuegos'});
+escuelaVgs.addCourses(
+  'Curso de Introducción a la Producción de Videojuegos',
+  'Curso de Unreal Engine',
+  'Curso de Unity 3D'
+);
+const escuelaAF = new LearningPath({name: 'Escuela de Astrofísica'});
+escuelaAF.addCourses(
+  'Curso de Física',
+  'Curso de Astronomía',
+  'Curso de Física Cuántica'
+);
+
+class Student {
+  constructor ({
+    name,
+    email,
+    username,
+    twitter = undefined,
+    instagram = undefined,
+    facebook = undefined,
+    approvedCourses = [],
+    learningPaths = []
+  }) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.socialMedia = {
+      twitter,
+      instagram,
+      facebook
+    };
+    this.approvedCourses = approvedCourses;
+    this.learningPaths = learningPaths;
+  }
+}
+
+const victor = new Student({
+  name: 'Victor Hugo',
+  email: 'ortizrvh@gmail.com',
+  username: 'vHugo',
+  twitter: 'vHugo76',
+  learningPaths: [
+    escuelaWeb,
+    escuelaVgs
+  ]
+})
+
+const oliver = new Student({
+  name: 'Oliver Jared',
+  email: 'ortizgoj@gmail.com',
+  username: 'oJared',
+  instagram: 'oJared04',
+  learningPaths: [
+    escuelaAF,
+    escuelaVgs
+  ]
+})
