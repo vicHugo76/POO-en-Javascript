367544efd4de6f91f79acb6bd5101b454ce43d83
Victor Hugo <viccorrt@gmail.com>
2023-06-11 23:41:49 -0700

    Analizamos las propiedades del prototipo Object que afectan a las propiedades de los objetos, como configutable, writable y enumerable, asi como los metodos estaticos seal y freeze

--- medium/static/main.js
+++ medium/static/main.js
@@ -13,11 +13,37 @@ const victor = {
 // console.log(Object.getOwnPropertyNames(victor)); // prop.static
 // console.log(Object.entries(victor)); // prop.static
 
-// console.log(Object.getOwnPropertyDescriptors(victor)); // prop. static
+// Object.seal(victor); // Pone en false, la propiedad configurable de todas propiedades del Objeto, osea no se puede borrar las propiedades del objeto
 
-Object.defineProperty(victor, 'pruebaNASA', {
-  value: 'extraterrestres',
-  enumerable: true,
-  writable: true,
-  configurable: true
-});
+// Object.freeze(victor); // Pone en false, las propiedades configurable y writable de todas propiedades del Objeto, osea no se puede borrar las propiedades del objeto ni cambiar su valor
+
+console.log(Object.getOwnPropertyDescriptors(victor)); // prop. static
+
+// Object.defineProperty(victor, 'navigator', {
+//   value: 'Chrome',
+//   enumerable: false,
+//   writable: true,
+//   configurable: true
+// });
+
+// Object.defineProperty(victor, 'editor', {
+//   value: 'VSCode',
+//   enumerable: true,
+//   writable: false,
+//   configurable: true
+// });
+
+// Object.defineProperty(victor, 'terminal', {
+//   value: 'WSL',
+//   enumerable: true,
+//   writable: true,
+//   configurable: false
+// });
+
+// Object.defineProperty(victor, 'pruebaNASA', {
+//   value: 'extraterrestres',
+//   enumerable: false,
+//   writable: false,
+//   configurable: false
+// });
+
