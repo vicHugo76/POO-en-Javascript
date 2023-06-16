79cf0e054a7fda5f0cf088d1bd523ef10ae85c3e
Victor Hugo <viccorrt@gmail.com>
2023-06-15 22:06:58 -0700

    Realizamos el ejercicio de DeepFreeze para profundizar en la recursividad  en JavaScript

---
+++ medium/playgrounds/deepFreeze/main.js
@@ -0,0 +1,44 @@
+// Dado un objeto como el siguiente:
+
+const juan = {
+  name: "Juanito",
+  approvedCourses: ["Curso 1","Curso 2"],
+  caracteristicas: {
+    age: 18,
+    colorCabello: 'Negro',
+    gustos: {
+      musica: ['rock', 'punk', 'ska'],
+      peliculas: ['drama', 'horror', 'comedia'],
+    },
+  },
+  addCourse(newCourse) {
+    console.log("This", this);
+    console.log("This.approvedCourses", this.approvedCourses);
+    this.approvedCourses.push(newCourse);
+  }
+};
+
+// Tu reto es crear una función que aplique Object.freeze a todos los objetos anidados de forma recursiva para así realmente lograr bloquear todo el objeto. A esto se le conoce comunmente como deepFreeze.
+
+function deepFreeze(obj) {
+  if (typeof obj !== 'object') 
+    return
+  Object.freeze(obj); 
+  for(let key in obj){
+    deepFreeze(obj[key]) 
+  }
+} 
+
+// function deepFreeze(obj) {
+//   if (typeof obj !== 'object' || obj === null) {
+//     return;
+//   }
+//   const props = Object.getOwnPropertyNames(obj);
+//   Object.freeze(obj);
+//   for (let prop of props) {
+//     const value = obj[prop];
+//     deepFreeze(value);
+//   }
+// }
+
+deepFreeze(juan);
