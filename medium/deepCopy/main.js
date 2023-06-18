ab7aa17219d0f45ab8f5c1e0cb4cffa8af30188c
Victor Hugo <viccorrt@gmail.com>
2023-06-18 10:26:38 -0700

    Utilizamos la abstraccion con objetos literales y deepcopy

--- medium/deepCopy/main.js
+++ medium/deepCopy/main.js
@@ -1,15 +1,15 @@
-const obj1 = {
-  a: 'a',
-  b: 'b',
-  c: {
-    d: 'd',
-    e: 'e'
-  },
-  editA() {
-    this.a = 'AAAAAAAAAAAA';
-  },
-  f: ['a', 1, 'b', 3, 7, 'v', 15]
-};
+// const obj1 = {
+//   a: 'a',
+//   b: 'b',
+//   c: {
+//     d: 'd',
+//     e: 'e'
+//   },
+//   editA() {
+//     this.a = 'AAAAAAAAAAAA';
+//   },
+//   f: ['a', 1, 'b', 3, 7, 'v', 15]
+// };
 
 function isObject(subject) {
   return typeof subject === 'object';
@@ -42,3 +42,30 @@ function deepCopy(subject) {
   }
   return copySubject;
 }
+
+const studentBase = {
+  name: undefined,
+  email: undefined,
+  age: undefined,
+  approvedCourses: undefined,
+  learningPaths: undefined,
+  socialMedia: {
+    facebook: undefined,
+    instagram: undefined,
+    twitter: undefined
+  }
+}
+
+const victor = deepCopy(studentBase);
+
+// De esta manera definimos propiedad por propiedad de un objeto 
+// Object.defineProperty(victor, 'name', {
+//   value: 'Victor Hugo',
+//   configurable: false
+// });
+
+// Con este metodo lo que hacemos es proteger todas las propiedades del objeto para que no puedan ser borradas
+Object.seal(victor);
+
+console.log(Object.isSealed(victor)); // Verificamos si las propiedades del objeto estan bloqueadas
+console.log(Object.isFrozen(victor)); // Verificamos si el valor de las propiedades estan protegidas
